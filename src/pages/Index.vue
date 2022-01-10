b6e8a65d5455aa3ab44a71188ecb574d45abf007
Ayato (Shahil) <67567036+Shahil-Ayato@users.noreply.github.com>
2022-01-10 13:35:41 -0800

    Update Index.vue (#9)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -91,7 +91,7 @@
                   </v-card-title>
                   <v-card-text
                     class="text-center"
-                  >As of January 2021, Ryujinx has been tested on over 3,000 commercial titles: ~2,300 boot past menus and into gameplay, with approximately 1,500 of those being considered playable. See the compatibility list <a href="https://github.com/Ryujinx/Ryujinx-Games-List" rel="noopener" target="_blank">here</a>.</v-card-text>
+                  >As of September 2021, Ryujinx has been tested on nearly 3,400 titles: ~3,000 boot past menus and into gameplay, with approximately 2,400 of those being considered playable. See the compatibility list <a href="https://github.com/Ryujinx/Ryujinx-Games-List" rel="noopener" target="_blank">here</a>.</v-card-text>
                 </v-card>
               </v-col>
             </v-row>
