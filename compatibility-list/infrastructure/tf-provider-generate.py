6e68d6c901d83c4adfafb327fffa08dee930bee9
Retka <christopherpickard6@gmail.com>
2022-06-18 17:13:33 -0400

    Tweaked tf-provider-generate.py to delete old terraform artifacts, instead of having to do it manually before running. Renamed github-issue-webhooks.tf to github-issue-webhooks-api.tf.

--- compatibility-list/infrastructure/tf-provider-generate.py
+++ compatibility-list/infrastructure/tf-provider-generate.py
@@ -1,13 +1,31 @@
 from string import Template
 import argparse
 import json
+import os
+import shutil
 
+# Script arguments
 parser = argparse.ArgumentParser(description="testdesc")
 parser.add_argument("--var-file", required=True, type=str, help="Path to the .tfvars.json file for the environment you want to deploy.")
 args = parser.parse_args()
 
 var_file = args.var_file
 
+# Check provided file exists
+if not os.path.isfile(var_file): 
+  raise Exception(f'File `{var_file}` does not exist.')
+
+# Clean up any terraform stuff from previous runs
+if os.path.isdir(".terraform"):
+  shutil.rmtree(".terraform")
+
+if os.path.isfile(".terraform.lock.hcl"): 
+  os.remove(".terraform.lock.hcl")
+
+if os.path.isfile("provider.tf"): 
+  os.remove("provider.tf")
+
+# Read vars file
 with open(var_file, "r") as f:
     tfvars = json.load(f)
 
@@ -18,6 +36,7 @@ params = {
     "aws_profile": tfvars["aws_profile"]
 }
 
+# Write provder.tf from provider.tf.tmpl, using vars from vars file.
 with open('provider.tf.tmpl', 'r') as tmplFile:
     src = Template(tmplFile.read())
     result = src.substitute(params)
